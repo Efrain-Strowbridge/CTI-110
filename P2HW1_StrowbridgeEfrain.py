ffc95983c72108f375dc4b3e93f4b4c2748aa8c5
Efrain-Strowbridge <strowbre9687@student.faytechcc.edu>
2026-09-25 20:39:50 -0400

    Vacation Budget Calculation and Display
    + New print formatting

---
+++ P2HW1_StrowbridgeEfrain.py
@@ -0,0 +1,28 @@
+# Efrain Strowbridge
+# 9/25/26
+# P2HW1
+# A brief description of the project
+
+
+# These constants calculate the travel expenses according to the user's input.
+print("This Program calculates and displays travel epenses!")
+Budget = float(input(f"Enter your Budget: "))
+Destination = input("Enter your Destination: ")
+Gas = float(input(f"How much do you plan to spend on gas: "))
+Living = float(input(f"How much do you plan to spend on hotel/housing: "))
+Food = float(input(f"How much do you need for food: "))
+Remaining_Budget = Budget-(Gas+Living+Food)
+
+def main():
+    # This function executes the program by using the user's input to display the travel expenses.
+    print("--------------Travel Expenses--------------")
+    print(f'{"Location: ":30}{Destination}')
+    print(f'{"Initial Budget: ":30}${Budget:.2f}')
+    print(f'{"Gas Budget: ":30}${Gas:.2f}')
+    print(f'{"Housing/Accomodation Budget: ":30}${Living:.2f}')
+    print(f'{"Food Budget: ":30}${Food:.2f}')
+    print("-------------------------------------------")
+    print()
+    print(f'{"Remaining Balance: ":30}${Remaining_Budget:.2f}')
+
+main()
